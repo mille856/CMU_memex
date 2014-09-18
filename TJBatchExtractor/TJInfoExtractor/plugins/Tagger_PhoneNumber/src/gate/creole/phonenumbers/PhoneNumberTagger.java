3ca26176eee28afa1c460f36d4cd09e233688c1e
Kyle Miller <mille856@andrew.cmu.edu>
2014-09-18 15:53:42 -0400

    updated license

--- TJBatchExtractor/TJInfoExtractor/plugins/Tagger_PhoneNumber/src/gate/creole/phonenumbers/PhoneNumberTagger.java
+++ TJBatchExtractor/TJInfoExtractor/plugins/Tagger_PhoneNumber/src/gate/creole/phonenumbers/PhoneNumberTagger.java
@@ -8,13 +8,16 @@
 */
 
 /* 
- * This file is free software,
- * licenced under the GNU Library General Public License, Version 3, June 2007
+ * This file is free software,
+
+ * licenced under the GNU Lesser General Public License, Version 3, June 2007
+
  * (in the distribution as file licence.html)
  *
- * This file was created by modifying the Tagger_Number plugin distributed with GATE (see http://gate.ac.uk/)
+ * This file was created by modifying the Tagger_Number plugin distributed with GATE (see http://gate.ac.uk/) under the same license (see https://gate.ac.uk/gate/licence.html as accessed July, 2014)
  * 
  */
+
 
 package phonenumbers;
 
